38d7ce5ef3f2c3a2db6d3927dcd4b2fcf9b36710
CORENdot <104617122+CORENdot@users.noreply.github.com>
2022-04-29 13:43:38 +0200

    Docu redirected to GitHub. Cleanup.

--- C3DOBJProcessor.cs
+++ C3DOBJProcessor.cs
@@ -8,85 +8,17 @@ using UnityEditor;
 using UnityEngine;
 
 /*
-    C3DOBJProcessor script works after .obj models are imported 
-    or modified within the Unity Editor.
+    C3DOBJProcessor script loads Vertex Colors from .obj files created with Crocotile3D.
 
-    Intention is to extend Unity default model importer to support 
-    Vertex Colors from .obj files created with Crocotile3D perfectly.
+    Read all Considerations and Limitations carefully from GitHub readme if you get a case
+    where it doesn't work properly:
+    https://github.com/CORENdot/Unity-Vertex-Color-Processor-for-Crocotile3D-.obj-files
 
-    It also changes "Scene" naming (non-object-tiles default group of Crocotile3D)
-    to filename_Scene, otherwise as the Unity Project grows it becomes impossible to 
-    search for individual Meshes if they are all called "Scene".
-
-    Tested on 2018.4.36f1 LTS, newer versions might need code changes due to deprecations.
-
-    CONSIDERATIONS AND LIMITATIONS:
-    1 - Crocotile3D Object hierarchy needs to be alphanumerically ordered by YOU!
-            - Only Objects, Instances' order don't matter.
-            - Order them descendingly.
-            - Pair equally named Objects together in the hierarchy, yet acknowledge
-                that they will get merged into a single Mesh by Unity.
-            - Layer names don't matter but the overall Object hierarchy still
-            needs to be ordered.
-            - Crocotile3D Hierarchy Examples:
-                - Example of CORRECT Crocotile3D hierarchy:
-                    - Superlayer Z
-                        - Object A
-                        - Object A
-                        - Object B
-                    - Object C
-                - Example of INCORRECT Crocotile3D hierarchy:
-                    - Superlayer Z
-                        - Object A
-                        - Object A
-                        - Object C
-                    - Object B
-            - I have found no way to workaround this limitation, it probably needs
-                a whole ModelImporter created from scratch which is out of my scope.
-
-    2 - Don't call any Crocotile3D Object exactly "Scene", including case variations.
-            - "Scene" is the default group (non-object-tiles group) of Crocotile3D
-                the order where the vertex data ends if an Object is called "Scene"
-                gets hard to be determined in many cases. Avoid it.
-
-    3 - If you have done the above steps and still get an error like "Mesh.colors is out of bounds"
-        it might be cause your Crocotile3D project has intersecting vertices.
-            - Try exporting the .obj file with Merge Vertices checkbox disabled in Crocotile3D
-            export settings.
-
-    4 - This script cannot distinguish .obj files created with Crocotile3D than other
-        modeling softwares. If you have .obj files created from other
-        modeling software imported or modified, THIS SCRIPT WILL PROBABLY FAIL!
-            - Feel free to extend the code and add conditions that will prevent the script to 
-            affect each .obj files of your project. 
-            - Example: Name your Croco .obj files as filename_c3d.obj and add a condition check 
-            for that "c3d" in code.
-
-    5 - If the Unity project doesn't have Color Space set to Gamma in the Player Settings 
-        vertex colors will result different than Crocotile 3D. (More subtle)
-            - Gamma is the default Color Space on new projects by Unity.
-            - There might be a workaround in ModelImporter functions to avoid this, check documentation.
-            - Other solution might involve to change how the Colors are parsed in this script. 
-
-    6 - It requires PreserveHierarchy, OptimizeMesh and WeldVertices from ModelImporter settings set to false
-        so Unity doesn't mismatch with the .obj file vertex order. 
-        (This script does this automatically for you on the OnPreprocessModel () function)
-
-    7 - Performance on multiple .obj importing or modifications has not been extensively tested.
-
-    8 - USE THIS AT YOUR OWN RISK! Backup your .obj files if you have no way of reimport them before
-        adding this script to your Unity project!
-
-    9 - Feel free to use this script however you want, yet remember that the original intention was
-        to help Unity and Crocotile3D users for FREE.
-
-    This is my gift for the Crocotile3D community, you can catch me on the official discord.
-        - CORENdot
+    - CORENdot
 */
 public sealed class C3DOBJProcessor : AssetPostprocessor
 {
 
-    private ModelImporter _modelImporter;
     private string _fileName;
     private string _extension;
     private string _fileFullPath;
@@ -99,10 +31,10 @@ public sealed class C3DOBJProcessor : AssetPostprocessor
 
         if (!File.Exists (_fileFullPath) || !_extension.Equals (".obj")) return;
 
-        _modelImporter = (ModelImporter) assetImporter;
-        _modelImporter.preserveHierarchy = false;
-        _modelImporter.optimizeMesh = false;
-        _modelImporter.weldVertices = false;
+        ModelImporter modelImporter = (ModelImporter) assetImporter;
+        modelImporter.preserveHierarchy = false;
+        modelImporter.optimizeMesh = false;
+        modelImporter.weldVertices = false;
         /* 
             You can add extra modelImporter settings here. 
             Bear in mind Preserve Hierarchy, Optimize Mesh, and Weld Vertices
@@ -128,7 +60,10 @@ public sealed class C3DOBJProcessor : AssetPostprocessor
             meshFilters.Insert (0, scene);
         }
 
-        // Search the .obj file for first "v" line position to start reading there.
+        /*
+            Search the .obj file for first "v" line position to start reading there.
+            Lines that start with "v" hold each vertex data.
+        */
         int currFileLine = 0;
         for (int i = 0; i < lines.Length && currFileLine == 0; i++)
         {
